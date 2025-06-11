ebaaa6c4e1ede7965ac45f24db977f2171b1b503
Pranav8722 <pranavbadaskar06@gmail.com>
2025-06-11 13:15:37 +0530

    addet exception handling

--- mocha/promises/async_n_await.js
+++ mocha/promises/async_n_await.js
@@ -1,7 +1,12 @@
 async function fetchdata(){
-    let data =await fetch("https://jsonplaceholder.typicode.com/users")
+    try{
+        let data =await fetch("https://jsonplaceholder.typicode.com/users")
     res=await data.json()
     console.log(res)
+    }
+    catch(error){
+        console.log("something went wrong",error)
+    }
 }
 
 fetchdata()
