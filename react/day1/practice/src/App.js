e4feca282827ca13957ae8d19de3ad67d0a2f44c
Pranav8722 <pranavbadaskar06@gmail.com>
2025-06-20 21:56:20 +0530

    started the react

---
+++ react/day1/practice/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    <h1>fruts list</h1>
+    <ul>
+      <li>banana</li>
+      <li>mango</li>
+      <li>papaya</li>
+    </ul>
+
+
+    </div>
+
+    <div className="form">
+    <form>
+      <label for="name">name:</label>
+      <input placeholder='write your name' id='name' type='text'></input>
+    </form>
+
+    <div className="parrent">
+        <div  className="c1">1</div>
+        <div className="c2">2</div>
+        <div className="c3">3</div>
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
